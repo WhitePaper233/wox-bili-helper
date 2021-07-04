b52667ff1ca48c6a15c0e306565abdae00289c2e
WhitePaper_ <30739658+AngelicaRoot@users.noreply.github.com>
2021-07-04 18:34:02 +0800

    Now support using prefix like "av" "bv" "cv" "ss"(regardless of case) and so on to open pages directly

--- plugin.py
+++ plugin.py
@@ -61,8 +61,67 @@ class Main(Wox):
                 }
             }
         ]
+
         if key != '':
-            result = [
+            result = []
+            if key[0:2] in ['av', 'AV', 'bv', 'BV', 'cv', 'CV', 'au', 'AU', 'ss', 'SS'] and len(key.replace(' ', '')) > 2:
+                if key[0:2] in ['av', 'AV', 'bv', 'BV']:
+                    result.append(
+                    {
+                        "Title": "直接打开视频",
+                        "SubTitle": f'''直接打开视频 {key}''',
+                        "IcoPath": "bili.ico",
+                        "ContextData": "ctxData",
+                        "JsonRPCAction":{
+                            'method': 'OpenPage',
+                            'parameters': [f'https://www.bilibili.com/video/{key}'],
+                            'dontHideAfterAction': False
+                        }
+                    }
+                    )
+                elif key[0:2] in ['cv', 'CV']:
+                    result.append(
+                    {
+                        "Title": "直接打开专栏",
+                        "SubTitle": f'''直接打开专栏 {key}''',
+                        "IcoPath": "bili.ico",
+                        "ContextData": "ctxData",
+                        "JsonRPCAction":{
+                            'method': 'OpenPage',
+                            'parameters': [f'https://www.bilibili.com/read/{key}'],
+                            'dontHideAfterAction': False
+                        }
+                    }
+                    )
+                elif key[0:2] in ['au', 'AU']:
+                    result.append(
+                    {
+                        "Title": "直接打开音频",
+                        "SubTitle": f'''直接打开音频 {key}''',
+                        "IcoPath": "bili.ico",
+                        "ContextData": "ctxData",
+                        "JsonRPCAction":{
+                            'method': 'OpenPage',
+                            'parameters': [f'https://www.bilibili.com/audio/{key}'],
+                            'dontHideAfterAction': False
+                        }
+                    }
+                    )
+                elif key[0:2] in ['ss', 'SS']:
+                    result.append(
+                    {
+                        "Title": "直接打开剧集",
+                        "SubTitle": f'''直接打开剧集 {key}''',
+                        "IcoPath": "bili.ico",
+                        "ContextData": "ctxData",
+                        "JsonRPCAction":{
+                            'method': 'OpenPage',
+                            'parameters': [f'https://www.bilibili.com/bangumi/play/{key}'],
+                            'dontHideAfterAction': False
+                        }
+                    }
+                    )
+            result.append(
             {
                 "Title": "搜索",
                 "SubTitle": f'''Bilibili搜索"{key}"的结果''',
@@ -74,7 +133,7 @@ class Main(Wox):
                     'dontHideAfterAction': False
                 }
             }
-            ]
+            )
         return result
 
     def OpenPage(self, url):
