c20ab1bb98aeaac86e3ba7c84a1535a7b244dee4
AngelicaRoot_na <1423634265@qq.com>
2021-07-07 18:20:12 +0800

    Code logic optimization

--- utils/generators/resultGenerator.py
+++ utils/generators/resultGenerator.py
@@ -1,151 +1,154 @@
 # -*-coding:utf-8-*-
-def generate_normal_result(title, subtitle, prefix, suffix='') -> dict:
-    dictionary = {
-        "Title": title,
-        "SubTitle": subtitle,
-        "IcoPath": "icons/bili.ico",
-        "ContextData": "ctxData",
-        "JsonRPCAction": {
-            'method': 'open_page',
-            'parameters': ['https://{}.bilibili.com/{}'.format(prefix, suffix)],
-            'dontHideAfterAction': False
+class GetResult:
+
+    @staticmethod
+    def generate_normal_result(title, subtitle, prefix, suffix='') -> list:
+        dictionary = {
+            "Title": title,
+            "SubTitle": subtitle,
+            "IcoPath": "icons/bili.ico",
+            "ContextData": "ctxData",
+            "JsonRPCAction": {
+                'method': 'open_page',
+                'parameters': ['https://{}.bilibili.com/{}'.format(prefix, suffix)],
+                'dontHideAfterAction': False
+            }
         }
-    }
-    return dictionary
+        return [dictionary]
 
-
-def generate_search_result(key) -> dict:
-    dictionary = {
-        "Title": "搜索:{}".format(key),
-        "SubTitle": '''在bilibili.com上搜索"{}"的结果'''.format(key),
-        "IcoPath": "icons/search.ico",
-        "ContextData": "ctxData",
-        "JsonRPCAction": {
-            'method': 'open_page',
-            'parameters': ['https://search.bilibili.com/all?keyword={}'.format(key)],
-            'dontHideAfterAction': False
+    @staticmethod
+    def generate_search_result(key) -> list:
+        dictionary = {
+            "Title": "搜索:{}".format(key),
+            "SubTitle": '''在bilibili.com上搜索"{}"的结果'''.format(key),
+            "IcoPath": "icons/search.ico",
+            "ContextData": "ctxData",
+            "JsonRPCAction": {
+                'method': 'open_page',
+                'parameters': ['https://search.bilibili.com/all?keyword={}'.format(key)],
+                'dontHideAfterAction': False
+            }
         }
-    }
-    return dictionary
+        return [dictionary]
 
-
-def generate_video_result(*args) -> dict:
-    """
-    :param args:{
-                [0]: title
-                [1]: up's name
-                [2]: view
-                [3]: like
-                [4]: favorite
-                [5]: danmaku
-                [6]: reply
-                [7]: aid or bvid
-    :return: search result
-    """
-    args = args[0]
-    dictionary = {
-        "Title": args[0],
-        "SubTitle": '''UP主:{}  播放:{}  点赞:{}  收藏:{}  弹幕:{}  评论:{}'''.format
-        (args[1], args[2], args[3], args[4], args[5], args[6]),
-        "IcoPath": "icons/bili.ico",
-        "ContextData": "ctxData",
-        "JsonRPCAction": {
-            'method': 'open_page',
-            'parameters': ['https://www.bilibili.com/video/{}'.format(args[7])],
-            'dontHideAfterAction': False
+    @staticmethod
+    def generate_video_result(*args) -> list:
+        """
+        :param args:{
+                    [0]: title
+                    [1]: up's name
+                    [2]: view
+                    [3]: like
+                    [4]: favorite
+                    [5]: danmaku
+                    [6]: reply
+                    [7]: aid or bvid
+        :return: search result
+        """
+        args = args[0]
+        dictionary = {
+            "Title": args[0],
+            "SubTitle": '''UP主:{}  播放:{}  点赞:{}  收藏:{}  弹幕:{}  评论:{}'''.format
+            (args[1], args[2], args[3], args[4], args[5], args[6]),
+            "IcoPath": "icons/bili.ico",
+            "ContextData": "ctxData",
+            "JsonRPCAction": {
+                'method': 'open_page',
+                'parameters': ['https://www.bilibili.com/video/{}'.format(args[7])],
+                'dontHideAfterAction': False
+            }
         }
-    }
-    return dictionary
+        return [dictionary]
 
-
-def generate_article_result(*args) -> dict:
-    """
-    :param args:{
-                [0]: title
-                [1]: up's name
-                [2]: view
-                [3]: favorite
-                [4]: like
-                [5]: reply
-                [6]: share
-                [7]: coin
-                [8]: aid or bvid
-    :return: search result
-    """
-    args = args[0]
-    dictionary = {
-        "Title": args[0],
-        "SubTitle": '''UP主:{}  浏览:{}  收藏:{}  点赞:{}  回复:{}  分享:{}  投币{}'''.format
-        (args[1], args[2], args[3], args[4], args[5], args[6], args[7]),
-        "IcoPath": "icons/bili.ico",
-        "ContextData": "ctxData",
-        "JsonRPCAction": {
-            'method': 'open_page',
-            'parameters': ['https://www.bilibili.com/read/{}'.format(args[8])],
-            'dontHideAfterAction': False
+    @staticmethod
+    def generate_article_result(*args) -> list:
+        """
+        :param args:{
+                    [0]: title
+                    [1]: up's name
+                    [2]: view
+                    [3]: favorite
+                    [4]: like
+                    [5]: reply
+                    [6]: share
+                    [7]: coin
+                    [8]: aid or bvid
+        :return: search result
+        """
+        args = args[0]
+        dictionary = {
+            "Title": args[0],
+            "SubTitle": '''UP主:{}  浏览:{}  收藏:{}  点赞:{}  回复:{}  分享:{}  投币{}'''.format
+            (args[1], args[2], args[3], args[4], args[5], args[6], args[7]),
+            "IcoPath": "icons/bili.ico",
+            "ContextData": "ctxData",
+            "JsonRPCAction": {
+                'method': 'open_page',
+                'parameters': ['https://www.bilibili.com/read/{}'.format(args[8])],
+                'dontHideAfterAction': False
+            }
         }
-    }
-    return dictionary
+        return [dictionary]
 
-
-def generate_audio_result(*args) -> dict:
-    """
-    :param args:{
-                [0]: title
-                [1]: up's name
-                [2]: author
-                [3]: view
-                [4]: favorite
-                [5]: comment
-                [6]: share
-                [7]: aid or bvid
-    :return: search result
-    """
-    args = args[0]
-    dictionary = {
-        "Title": args[0],
-        "SubTitle": '''UP主:{}  歌手:{}  播放:{}  收藏:{}  评论:{}  分享:{}'''.format
-        (args[1], args[2], args[3], args[4], args[5], args[6]),
-        "IcoPath": "icons/bili.ico",
-        "ContextData": "ctxData",
-        "JsonRPCAction": {
-            'method': 'open_page',
-            'parameters': ['https://www.bilibili.com/audio/{}'.format(args[7])],
-            'dontHideAfterAction': False
+    @staticmethod
+    def generate_audio_result(*args) -> list:
+        """
+        :param args:{
+                    [0]: title
+                    [1]: up's name
+                    [2]: author
+                    [3]: view
+                    [4]: favorite
+                    [5]: comment
+                    [6]: share
+                    [7]: aid or bvid
+        :return: search result
+        """
+        args = args[0]
+        dictionary = {
+            "Title": args[0],
+            "SubTitle": '''UP主:{}  歌手:{}  播放:{}  收藏:{}  评论:{}  分享:{}'''.format
+            (args[1], args[2], args[3], args[4], args[5], args[6]),
+            "IcoPath": "icons/bili.ico",
+            "ContextData": "ctxData",
+            "JsonRPCAction": {
+                'method': 'open_page',
+                'parameters': ['https://www.bilibili.com/audio/{}'.format(args[7])],
+                'dontHideAfterAction': False
+            }
         }
-    }
-    return dictionary
+        return [dictionary]
 
-
-def generate_bangumi_result(*args) -> dict:
-    """
-    :param args:{
-                [0]: title
-                [1]: total
-                [2]: views
-                [3]: coins
-                [4]: danmakus
-                [5]: favorites
-                [6]: reply
-                [7]: share
-                [8]: ssid
-    :return: search result
-    """
-    args = args[0]
-    dictionary = {
-        "Title": args[0],
-        "SubTitle": '''总{}集  播放:{}  硬币:{}  弹幕:{}  收藏:{}  评论:{}  分享{}'''.format
-        (args[1], args[2], args[3], args[4], args[5], args[6], args[7]),
-        "IcoPath": "icons/bili.ico",
-        "ContextData": "ctxData",
-        "JsonRPCAction": {
-            'method': 'open_page',
-            'parameters': ['https://www.bilibili.com//bangumi/play/ss{}'.format(args[8])],
-            'dontHideAfterAction': False
+    @staticmethod
+    def generate_bangumi_result(*args) -> list:
+        """
+        :param args:{
+                    [0]: title
+                    [1]: total
+                    [2]: views
+                    [3]: coins
+                    [4]: danmakus
+                    [5]: favorites
+                    [6]: reply
+                    [7]: share
+                    [8]: ssid
+        :return: search result
+        """
+        args = args[0]
+        dictionary = {
+            "Title": args[0],
+            "SubTitle": '''总{}集  播放:{}  硬币:{}  弹幕:{}  收藏:{}  评论:{}  分享{}'''.format
+            (args[1], args[2], args[3], args[4], args[5], args[6], args[7]),
+            "IcoPath": "icons/bili.ico",
+            "ContextData": "ctxData",
+            "JsonRPCAction": {
+                'method': 'open_page',
+                'parameters': ['https://www.bilibili.com//bangumi/play/ss{}'.format(args[8])],
+                'dontHideAfterAction': False
+            }
         }
-    }
-    return dictionary
+        return [dictionary]
 
 
 if __name__ == '__main__':
-    print(generate_search_result(key='cv12032529'))
+    print(GetResult.generate_search_result(key='cv12032529'))
--- utils/interpreters/basicInterpreter.py
+++ utils/interpreters/basicInterpreter.py
@@ -7,52 +7,55 @@ class BasicInterpreter:
     @staticmethod
     def interpret(key: str):
         if key == '':
-            return [
-                # The default frequently-used pages
-                # TODO: Customize pages
-                generate_normal_result('首页', '打开Bilibili首页', 'www'),
-                generate_normal_result('动态', '打开Bilibili动态页', 't'),
-                generate_normal_result('个人主页', '打开Bilibili个人主页', 'space'),
-                generate_normal_result('直播', '打开Bilibili直播页', 'live'),
-                generate_normal_result('漫画', '打开Bilibili漫画', 'manga')
-            ]
+            # The default frequently-used pages
+            # TODO: Customize pages
+            return_list = []
+            return_list.extend(GetResult.generate_normal_result('首页', '打开Bilibili首页', 'www'))
+            return_list.extend(GetResult.generate_normal_result('动态', '打开Bilibili动态页', 't'))
+            return_list.extend(GetResult.generate_normal_result('个人主页', '打开Bilibili个人主页', 'space'))
+            return_list.extend(GetResult.generate_normal_result('直播', '打开Bilibili直播页', 'live'))
+            return_list.extend(GetResult.generate_normal_result('漫画', '打开Bilibili漫画', 'manga'))
+            return return_list
 
         elif key.startswith('-'):
-            return [
-                generate_search_result(key)
-            ]
+            return GetResult.generate_search_result(key)
             # TODO: Command interpret
 
         elif key[0:2].lower() not in ['av', 'bv', 'cv', 'au', 'ss']:
             # Regular Search
-            return [generate_search_result(key)]
+            return GetResult.generate_search_result(key)
 
         else:
             prefix = key[0:2].lower()
             result = []
             if prefix == 'av' and key.replace(key[0:2], '').isdecimal():
                 # Advanced searching by using aid
-                result.append(generate_video_result(idInterpreter.IDInterpreter.id_interpreter(id_type='aid', key=key)))
+                result.extend(GetResult.generate_video_result(
+                    idInterpreter.IDInterpreter.id_interpreter(id_type='aid', key=key)))
 
             elif prefix == 'bv' and ' ' not in key.replace(key[0:2], '', 1) and len(key) >= 3:
                 # Advanced searching by using bvid
-                result.append(generate_video_result(idInterpreter.IDInterpreter.id_interpreter(id_type='bvid', key=key)))
+                result.extend(GetResult.generate_video_result(
+                    idInterpreter.IDInterpreter.id_interpreter(id_type='bvid', key=key)))
 
             elif prefix == 'cv' and key.replace(key[0:2], '').isdecimal():
                 # Advanced searching by using cid
-                result.append(generate_article_result(idInterpreter.IDInterpreter.id_interpreter(id_type='cvid', key=key)))
+                result.extend(GetResult.generate_article_result(
+                    idInterpreter.IDInterpreter.id_interpreter(id_type='cvid', key=key)))
 
             elif prefix == 'au' and key.replace(key[0:2], '').isdecimal():
                 # Advanced searching by using auid
-                result.append(generate_audio_result(idInterpreter.IDInterpreter.id_interpreter(id_type='auid', key=key)))
+                result.extend(GetResult.generate_audio_result(
+                    idInterpreter.IDInterpreter.id_interpreter(id_type='auid', key=key)))
 
             elif prefix == 'ss' and key.replace(key[0:2], '').isdecimal():
                 # Advanced searching by using ssid
-                result.append(generate_bangumi_result(idInterpreter.IDInterpreter.id_interpreter(id_type='ssid',key=key)))
+                result.extend(GetResult.generate_bangumi_result(
+                    idInterpreter.IDInterpreter.id_interpreter(id_type='ssid', key=key)))
 
-            result.append(generate_search_result(key))
+            result.extend(GetResult.generate_search_result(key))
             return result
 
 
 if __name__ == '__main__':
-    print(BasicInterpreter.interpret('Cv4890667'))
+    print(BasicInterpreter.interpret('av10492'))
